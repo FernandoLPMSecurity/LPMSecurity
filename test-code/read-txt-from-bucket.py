eb7cad04bb913a62dfcc7a33ccefea6c50bddde5
user <lpm.ctf.core2022@gmail.com>
2022-08-19 18:10:30 -0500

    Cleaned source code

--- test-code/read-txt-from-bucket.py
+++ test-code/read-txt-from-bucket.py
@@ -1,10 +1,7 @@
 import boto3
 
-# Test 1: Read txt file from S3 Bucket.
-# TODO: Before pushing to GH, dont forget to delete creds and comments
-
-ACCESS_KEY = "AKIAWFERYI3C3YQ3LGQG"
-SECRET_KEY = "0BhpZLexpfTpoJY+X/W7iFMIqXYe3dY5XfPOQ0zp"
+ACCESS_KEY = "REDACTED"
+SECRET_KEY = "REDACTED"
 
 s3 = boto3.resource('s3', aws_access_key_id=ACCESS_KEY, aws_secret_access_key=SECRET_KEY, region_name='us-east-1')
 bucket = 'core-ctf-2022-secrets-1'
@@ -14,5 +11,3 @@ content = (obj.get()['Body'].read().decode('utf-8'))
 
 this_is_the_flag=content
 print(this_is_the_flag)
-
-# LPM{h4rdc0d1n6_cr3d3n714l5_15_n07_c00l_hum4n!}
