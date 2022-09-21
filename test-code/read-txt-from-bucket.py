2ecc1193be020046065e5039a63991c1b8aff8ed
user <lpm.ctf.core2022@gmail.com>
2022-09-21 14:18:26 -0500

    Changed bucket to read

--- test-code/read-txt-from-bucket.py
+++ test-code/read-txt-from-bucket.py
@@ -4,7 +4,7 @@ ACCESS_KEY = "REDACTED"
 SECRET_KEY = "REDACTED"
 
 s3 = boto3.resource('s3', aws_access_key_id=ACCESS_KEY, aws_secret_access_key=SECRET_KEY, region_name='us-east-1')
-bucket = 'core-ctf-2022-secrets-1'
+bucket = 'core-ctf-2022-secrets-2'
 key = 'flag.txt'
 obj = s3.Object(bucket, key)
 content = (obj.get()['Body'].read().decode('utf-8'))
